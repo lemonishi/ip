df32b86228ca817c9dcd55812d836fac0e34127e
Lennon <lemoncode8888@gmail.com>
2026-01-15 15:21:10 +0800

    (feat): accepts and echoes commands

--- src/main/java/YeetMan.java
+++ src/main/java/YeetMan.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
+
 public class YeetMan {
+
+    private static void handleIO() {
+        String line = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (!input.equals("bye")) {
+            while (!input.equals("bye")) {
+                String output = String.format("%s\n %s\n %s\n", line, input, line);
+                System.out.println(output);
+                input = scanner.nextLine();
+            }
+            System.out.println(line + "\nbye\n" + line);
+        } else {
+            System.out.println(line + "\nbye\n" + line);
+        }
+    }
+
     public static void main(String[] args) {
         String greeting = "____________________________________________________________\n" +
                 "Hello! I'm YeetMan. It's just me Uce!\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+                "____________________________________________________________";
         System.out.println(greeting);
+        handleIO();
     }
 }
 
