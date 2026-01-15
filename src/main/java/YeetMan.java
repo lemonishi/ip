d7c10c77eb93f93e3a828d575f746b0561997e7d
Lennon <lemoncode8888@gmail.com>
2026-01-15 22:08:40 +0800

    feat: added Task class and task status handling

--- src/main/java/YeetMan.java
+++ src/main/java/YeetMan.java
@@ -3,22 +3,31 @@ import java.util.Scanner;
 
 public class YeetMan {
 
-    private static String[] list = new String[100];
+    private static Task[] list = new Task[100];
     private static int count = 0;
     private static final String LINE = "____________________________________________________________";
 
     private static void handleIO() {
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        while (!input.equals("bye")) {
+        while (true) {
             input = scanner.nextLine();
-            if (input.equals("list")) {
+            String command = input.split(" ")[0];
+            if (command.equals("list")) {
                 outputDisplay(list);
-            } else if (input.equals("bye")) {
+            } else if (command.equals("bye")) {
                 System.out.println(LINE + "\nBYE! 4 LETTERS, 1 WORD. UH UH, YEET!\n" + LINE);
                 break;
+            } else if (command.equals("mark")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                list[index].markAsDone();
+                System.out.printf("%s\nGreat job Uce! I've marked this task as done: \n%s\n%s\n", LINE, list[index], LINE);
+            } else if (command.equals("unmark")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                list[index].markAsUndone();
+                System.out.printf("%s\nI've marked this task as not done yet: \n%s\n%s\n", LINE, list[index], LINE);
             } else {
-                list[count] = input;
+                list[count] = new Task(input);
                 count++;
                 String output = String.format("%s\n added: %s\n %s\n", LINE, input, LINE);
                 System.out.println(output);
@@ -26,12 +35,13 @@ public class YeetMan {
         }
     }
 
-    private static void outputDisplay(String[] list) {
+    private static void outputDisplay(Task[] list) {
         String outputList = "";
+        String header = "Here are the tasks in your list, Uce! :";
         for (int i = 0; i < count ; i++) {
             outputList += String.format("%d. %s\n", i + 1, list[i]);
         }
-        System.out.printf("%s\n %s %s\n", LINE, outputList, LINE);
+        System.out.printf("%s\n %s\n %s %s\n", LINE, header, outputList, LINE);
     }
 
     public static void main(String[] args) {
