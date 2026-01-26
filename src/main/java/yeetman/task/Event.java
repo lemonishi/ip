febd28aa1d0dd55c9f2c3ba57e27935b2986a452
Lennon <lemoncode8888@gmail.com>
2026-01-26 08:27:53 +0800

    docs: Add Javadoc comments for Task class and its subclasses

--- src/main/java/yeetman/task/Event.java
+++ src/main/java/yeetman/task/Event.java
@@ -3,10 +3,16 @@ package yeetman.task;
 import java.time.LocalDateTime;
 
 public class Event extends Task {
-
     private LocalDateTime startDate;
     private LocalDateTime endDate;
 
+    /**
+     * Instantiates a new Event instance.
+     *
+     * @param description Description of the event.
+     * @param startDate Start date of the event.
+     * @param endDate End date of the event.
+     */
     public Event(String description, LocalDateTime startDate, LocalDateTime endDate) {
         super(description);
         this.startDate = startDate;
