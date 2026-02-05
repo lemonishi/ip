9fbe9997a04beb46845fdd201b8872b23e1914ec
Lennon <lemoncode8888@gmail.com>
2026-02-05 10:36:52 +0800

    fix: Update windows bat file for ui test

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -11,15 +11,18 @@ if exist ..\data\yeetman_test.txt del ..\data\yeetman_test.txt
 echo. > ..\data\yeetman_test.txt
 
 REM compile the code into the bin folder
-for /f %%f in ('dir /s /b ..\src\main\java\*.java') do javac -Xlint:none -d ..\bin "%%f"
+dir /s /B ..\src\main\java\*.java > sources.txt
+javac -cp ..\src\main\java -Xlint:none -d ..\bin @sources.txt
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
+    del sources.txt
     exit /b 1
 )
+del sources.txt
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin YeetMan < input.txt > ACTUAL.TXT
+java -classpath ..\bin yeetman.YeetMan ..\data\yeetman_test.txt < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
